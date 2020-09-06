039e72f39e0f0780d076a704118020c081ba9f4d
tu <nguyenmanhtu09042000@gmail.com>
2020-09-06 15:48:59 +0700

    update

---
+++ db/migrate/20200906055309_add_fields_to_u_sers.rb
@@ -0,0 +1,8 @@
+class AddFieldsToUSers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :address, :string
+    add_column :users, :phone, :interger
+  end
+end
